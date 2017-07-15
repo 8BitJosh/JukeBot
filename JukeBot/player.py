9223fb9935bd2394198d4e9b2d0433dac2dcbab1
Josh <joshuadp117@gmail.com>
2017-07-15 22:00:17 +0100

    Replaced tabs for spaces

--- JukeBot/player.py
+++ JukeBot/player.py
@@ -8,36 +8,36 @@ import os
 class Player:
 #play song located at path kill current song first if running
 #might add pipe stuff? for stdin commands
-	def __init__(self, path):
-		self.p = subprocess.Popen(
-							['ffplay', '-nodisp', '-autoexit', path],
-							#shell = True
-							stdin=subprocess.PIPE, 
-							stdout=subprocess.PIPE, 
-							stderr=subprocess.STDOUT)
+    def __init__(self, path):
+        self.p = subprocess.Popen(
+                            ['ffplay', '-nodisp', '-autoexit', path],
+                            #shell = True
+                            stdin=subprocess.PIPE, 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.STDOUT)
 
-		if self.p.poll() is None:
-			print("poll true")
-	
+        if self.p.poll() is None:
+            print("poll true")
+    
 #check if the song is still running
-	def running(self):
-		#print("is it running")
-		#for line in iter(self.p.stdout.readline, b''):
-		#	print( '>>> {}'.format(line))
-		return True
-		#process.poll() to check if subprocess is still running
-	
+    def running(self):
+        #print("is it running")
+        #for line in iter(self.p.stdout.readline, b''):
+        #    print( '>>> {}'.format(line))
+        return True
+        #process.poll() to check if subprocess is still running
+    
 #stop current song and cancel playback
-	def stop(self):
-		print("stop")
-	
+    def stop(self):
+        print("stop")
+    
 #pause the current song could pipe stdin to ffmpeg 'p' pauses/resumes
-	def pause(self):
-		print("pause")
-	
+    def pause(self):
+        print("pause")
+    
 #resume the current song if it has been resumed
-	def resume(self):
-		print("resume")
+    def resume(self):
+        print("resume")
 
 # Get duration with ffprobe
 #   ffprobe.exe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal filename.mp3
