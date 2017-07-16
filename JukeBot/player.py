510a20823b2a605cf1d7b968d25574d415e7b76b
Josh <joshuadp117@gmail.com>
2017-07-16 02:09:24 +0100

    Basic player works as intended
    still lots of bugs and features to be added but a song can be played and the player reports when the song has finished so it moves to the next song in the playlist

--- JukeBot/player.py
+++ JukeBot/player.py
@@ -1,31 +1,28 @@
 import subprocess
 import os
 
-#vce.create_ffmpeg_player(path, options='''-filter:a "volume={}"'''.format(volume))
-# ^^^ original music bot help for filter/volume
-#-af dynaudnorm       <- normalization filter ?
+#to delete file after finished with it
+#os.remove("filename.mp3")
 
 class Player:
 #play song located at path kill current song first if running
-#might add pipe stuff? for stdin commands
-    def __init__(self, path):
+#might add pipe stuff? for stdin commands idk if this will work - have to give it a try
+    def __init__(self, path):  
         self.p = subprocess.Popen(
                             ['ffplay', '-nodisp', '-autoexit', path],
                             #shell = True
+                            stdout=subprocess.DEVNULL, 
                             stdin=subprocess.PIPE, 
-                            stdout=subprocess.PIPE, 
-                            stderr=subprocess.STDOUT)
+                            stderr=subprocess.STDOUT
+                            )             
 
-        if self.p.poll() is None:
-            print("poll true")
-    
 #check if the song is still running
     def running(self):
-        #print("is it running")
-        #for line in iter(self.p.stdout.readline, b''):
-        #    print( '>>> {}'.format(line))
-        return True
-        #process.poll() to check if subprocess is still running
+        if self.p.poll() is None:
+            return True
+        else:
+            print("Finished")
+            return False      
     
 #stop current song and cancel playback
     def stop(self):
@@ -38,8 +35,3 @@ class Player:
 #resume the current song if it has been resumed
     def resume(self):
         print("resume")
-
-# Get duration with ffprobe
-#   ffprobe.exe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal filename.mp3
-# This is also how I fix the format checking issue for now
-# ffprobe -v quiet -print_format json -show_format stream
