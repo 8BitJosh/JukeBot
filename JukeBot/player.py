6d777ec46b1acee7ae576078d431fcb7f5f8994c
Josh <joshuadp117@gmail.com>
2017-08-14 02:21:17 +0100

    this should have been in prev commit :(

--- JukeBot/player.py
+++ JukeBot/player.py
@@ -2,17 +2,18 @@ import subprocess
 import os
 import time
 import traceback
-from utils import delete_file
+from utils import delete_file, PlaylistEntry
 
 class Player:
-#play song located at path
-#might add pipe stuff? for stdin commands idk if this will work - have to give it a try
+#create the player member
     def __init__(self):
-        pass
+        self.path = ''
+        self.dur = 0
 
-# start playing song at path
-    def play(self, _path):
-        self.path = _path
+#start playing song at path
+    def play(self, _song):
+        self.path = _song.dir
+        self.dur = _song.duration
         self.p = subprocess.Popen(
                             ['avplay', '-nodisp', '-autoexit', self.path],
                             #shell = True
@@ -20,7 +21,30 @@ class Player:
                             stdin=subprocess.PIPE, 
                             stderr=subprocess.STDOUT
                             )
+        self.startTime = time.time()
+        self.newSong = True
         print("playing - " + self.path)
+
+#has a new song started playing
+    def newsong(self):
+        if self.newSong:
+            self.newSong = False
+            return True
+        else:
+            return False
+
+#get song duration and current position
+    def getDuration(self):
+        durData = {}
+        
+        durData['dur'] = self.dur
+
+        if self.running():
+            durData['pos'] = int(time.time() - self.startTime)
+        else:
+            durData['pos'] = 0
+
+        return durData
     
 #check if the song is still running
     def running(self):
@@ -31,6 +55,7 @@ class Player:
                 if self.path != '' and os.path.exists(self.path):
                     delete_file(self.path)
                 self.path = ''
+                self.dur = 0
                 return False
         except: # causes error before first song as no process is playing
             return False
@@ -41,6 +66,7 @@ class Player:
             self.p.kill()
             delete_file(self.path)
             self.path = ''
+            self.dur = 0
         else:
             print("Unable to skip - no song playing")
 
